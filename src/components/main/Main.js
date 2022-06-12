ca357b6c644a62e129b856ed11ca4432b3a9a56a
Sene68 <senefull68@gmail.com>
2022-06-12 11:35:21 +0900

    main width 조정

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,15 +6,15 @@ function Main() {
     return(
         <div className="main">
             <Callout title={"Getting Started"}>
-                <h6 class="bp4-heading">1. React 프로젝트 생성 </h6>
+                <h6 className="bp4-heading">1. React 프로젝트 생성 </h6>
                 <p>
                     <Code>{"npx create-react-app blueprint-components"}</Code>
                 </p>
-                <h6 class="bp4-heading">2. Blueprint JS 설치 (Yarn 으로 진행)</h6>
+                <h6 className="bp4-heading">2. Blueprint JS 설치 (Yarn 으로 진행)</h6>
                 <p>
                     <Code>{"yarn add @blueprintjs/core"}</Code>
                 </p>
-                <h6 class="bp4-heading">3. Blueprint CSS 적용</h6>
+                <h6 className="bp4-heading">3. Blueprint CSS 적용</h6>
                 <p>
                     index.js에서 Blueprint CSS import 
                 </p>
