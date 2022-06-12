c27126a3d560b078c7a5b529f912dd521da01268
Sene68 <senefull68@gmail.com>
2022-06-12 11:28:47 +0900

    getting started 내용 작성

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,9 +6,21 @@ function Main() {
     return(
         <div className="main">
             <Callout title={"Getting Started"}>
-                The component is a simple wrapper around the CSS API that provides props for modifiers and optional
-                title element. Any additional HTML props will be spread to the rendered <Code>{"<div>"}</Code>{" "}
-                element.
+                <h6 class="bp4-heading">1. React 프로젝트 생성 </h6>
+                <p>
+                    <Code>{"npx create-react-app blueprint-components"}</Code>
+                </p>
+                <h6 class="bp4-heading">2. Blueprint JS 설치 (Yarn 으로 진행)</h6>
+                <p>
+                    <Code>{"yarn add @blueprintjs/core"}</Code>
+                </p>
+                <h6 class="bp4-heading">3. Blueprint CSS 적용</h6>
+                <p>
+                    index.js에서 Blueprint CSS import 
+                </p>
+                <p>
+                    <Code>{"import '@blueprintjs/core/lib/css/blueprint.css';"}</Code>
+                </p>
             </Callout>
         </div>
     );
