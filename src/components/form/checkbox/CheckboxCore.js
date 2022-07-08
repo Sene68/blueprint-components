eb6c474e2f8b126466d4bfcf2b1b8addad8ff5ca
Sene68 <senefull68@gmail.com>
2022-07-09 08:37:13 +0900

    checkbox example component 추가
    switch랑 공통으로 쓰도록 분리해야함

--- src/components/form/checkbox/CheckboxCore.js
+++ src/components/form/checkbox/CheckboxCore.js
@@ -1,9 +1,26 @@
-import * as React from "react";
-import { Callout, Card, Checkbox } from "@blueprintjs/core";
+import React, { useReducer } from "react";
+import { Alignment, Callout, Card, Checkbox, H5, Switch } from "@blueprintjs/core";
 import { CodeBlock, dracula } from "react-code-blocks";
+import { Example } from "@blueprintjs/docs-theme";
+import AlignmentSelect from "../../common/AlignmentSelect";
 import "../../main/Main.scss";
 
 function CheckboxCore() {
+    const initialState = {
+        alignIndicator: Alignment.LEFT,
+        disabled: false,
+        inline: false,
+        large: false
+    };
+
+    const [state, dispatch] = useReducer(Reducer, initialState);
+
+    const alignments = {
+        "Left" : Alignment.LEFT,
+        "Center" : Alignment.CENTER,
+        "Right" : Alignment.RIGHT 
+    };
+
     const CODE = 
     `
     import * as React from "react";
@@ -20,14 +37,44 @@ function CheckboxCore() {
     export default Checkbox;
     `;
 
+    const handleDisabledChange = () => {
+        dispatch({type: 'DisabledChange', value: !state.disabled});
+    };
+
+    const handleLargeChange = () => {
+        dispatch({type: 'LargeChange', value: !state.large});
+    }
+
+    const handleInlineChange = () => {
+        dispatch({type: 'InlineChange', value: !state.inline});
+    }
+
+    const handleAlignChange = (e) => {
+        const innerText = e.target.innerText;
+        const align = alignments[innerText];
+        dispatch({type: 'AlignChange', value: align});
+    }; 
+
+    const options = (
+        <>
+            <H5>Props</H5>
+            <Switch checked={state.disabled} label="Disabled" onChange={handleDisabledChange} />
+            <Switch checked={state.inline} label="Inline" onChange={handleInlineChange} />
+            <Switch checked={state.large} label="Large" onChange={handleLargeChange} />
+            <AlignmentSelect align={state.alignIndicator} onChange={handleAlignChange} />
+        </>
+    );
+
     return(
         <div className="main">
             <Callout title={"Checkbox"}>
-                <Card>
-                    <Checkbox label="Gilad Gray" defaultIndeterminate={true} />
-                    <Checkbox label="Jason Killian" />
-                    <Checkbox label="Antoine Llorca" />
-                </Card>
+                <Example options={options}>
+                    <div>
+                        <Checkbox {...state} label="Gilad Gray" defaultIndeterminate={true} />
+                        <Checkbox {...state} label="Jason Killian" />
+                        <Checkbox {...state} label="Antoine Llorca" />
+                    </div>
+                </Example>
                 <br/>
                 <h6 className="bp4-heading">Code </h6>
                 <p>
@@ -44,3 +91,23 @@ function CheckboxCore() {
 }
 
 export default CheckboxCore;
+
+function Reducer(state, action) {
+    switch (action.type) {
+        case 'DisabledChange': {
+            return {...state, disabled: action.value}
+        }
+
+        case 'LargeChange': {
+            return {...state, large: action.value}
+        }
+
+        case 'InlineChange': {
+            return {...state, inline: action.value}
+        }
+
+        case 'AlignChange': {
+            return {...state, alignIndicator: action.value}
+        }
+    }
+}
