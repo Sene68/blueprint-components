bde3d7a5f15ea77c9a2cedecbfedf51a894b121b
Sene68 <senefull68@gmail.com>
2022-07-13 19:48:03 +0900

    navbar example component 추가

--- src/components/core/navbar/NavbarCore.js
+++ src/components/core/navbar/NavbarCore.js
@@ -1,9 +1,13 @@
-import * as React from "react";
-import { Alignment, Button, Callout, Card, Classes, Navbar, NavbarDivider, NavbarGroup, NavbarHeading } from "@blueprintjs/core";
+import React, { useState } from "react";
+import { Alignment, Button, Callout, Classes, H5, Navbar, NavbarDivider, NavbarGroup, NavbarHeading, Switch } from "@blueprintjs/core";
+import { Example } from "@blueprintjs/docs-theme";
 import { CodeBlock, dracula } from "react-code-blocks";
 import "../../main/Main.scss";
 
 function NavbarCore() {
+    const [alignRight, setAlignRight] = useState(false);
+
+
     const CODE = 
     `
     import * as React from "react";
@@ -25,19 +29,30 @@ function NavbarCore() {
     export default Navbar;
     `;
 
+    const handleAlignRightChange = () => {
+        setAlignRight(!alignRight);
+    }
+
+    const options = (
+        <>
+            <H5>Props</H5>
+            <Switch checked={alignRight} label="Align right" onChange={handleAlignRightChange} />
+        </>
+    );
+
     return(
         <div className="main">
             <Callout title={"Navbar"}>
-                <Card>
+                <Example options={options}>
                     <Navbar>
-                        <NavbarGroup align={Alignment.LEFT}>
+                        <NavbarGroup align={alignRight ? Alignment.RIGHT : Alignment.LEFT}>
                             <NavbarHeading>Blueprint</NavbarHeading>
                             <NavbarDivider />
                             <Button className={Classes.MINIMAL} icon="home" text="Home" />
                             <Button className={Classes.MINIMAL} icon="document" text="Files" />
                         </NavbarGroup>
                     </Navbar>
-                </Card>
+                </Example>
                 <br/>
                 <h6 className="bp4-heading">Code </h6>
                 <p>
