93704343027fb35b996198335b0e31ac8c5aae06
Sene68 <senefull68@gmail.com>
2022-07-15 21:56:51 +0900

    spinner example component 추가

--- src/components/core/spinner/SpinnerCore.js
+++ src/components/core/spinner/SpinnerCore.js
@@ -1,9 +1,21 @@
-import * as React from "react";
-import { Callout, Card, Spinner } from "@blueprintjs/core";
+import React, { useReducer } from "react";
+import { Callout, H5, Intent, Label, Slider, Spinner, SpinnerSize, Switch } from "@blueprintjs/core";
 import { CodeBlock, dracula } from "react-code-blocks";
+import { Example } from "@blueprintjs/docs-theme";
+
+import IntentSelect from "../../common/IntentSelect";
 import "../../main/Main.scss";
 
 function SpinnerCore() {
+    const initialState = {
+        hasValue: false,
+        intent: Intent.NONE,
+        size: SpinnerSize.STANDARD,
+        value: 0.7
+    };
+
+    const [state, dispatch] = useReducer(Reducer, initialState);
+
     const CODE = 
     `
     import * as React from "react";
@@ -20,14 +32,67 @@ function SpinnerCore() {
     export default Spinner;
     `;
 
+    const handleIntentChange = (e) => {
+        const intent = e.target.value;
+        dispatch({type: 'IntentChange', value: intent});
+    };
+
+    const handleBooleanChange = () => {
+        dispatch({type: 'BooleanChange', value: !state.hasValue});
+    };
+
+    const handleSizeChange = (e) => {
+        const size = e;
+        dispatch({type: 'SizeChange', value: size});
+    };
+
+    const handleValueChange = (e) => {
+        const value = e;
+        dispatch({type: 'ValueChange', value: value});
+    };
+
+    const renderLabel = state.value.toFixed(1);
+
+    const options = (
+        <>
+                <H5>Props</H5>
+                <IntentSelect intent={state.intent} onChange={handleIntentChange} />
+                <Label>Size</Label>
+                <Slider
+                    labelStepSize={50}
+                    min={0}
+                    max={SpinnerSize.LARGE * 2}
+                    showTrackFill={false}
+                    stepSize={5}
+                    value={state.size}
+                    onChange={handleSizeChange}
+                />
+                <Switch checked={state.hasValue} label="Known value" onChange={handleBooleanChange} />
+                <Slider
+                    disabled={!state.hasValue}
+                    labelStepSize={1}
+                    min={0}
+                    max={1}
+                    onChange={handleValueChange}
+                    labelRenderer={renderLabel}
+                    stepSize={0.1}
+                    showTrackFill={false}
+                    value={state.value}
+                />
+        </>
+    );
+
     return(
         <div className="main">
             <Callout title={"Spinner"}>
-                <Card>
+                <Example options={options}>
                     <Spinner
-                        size={50}
+                        aria-label={state.hasValue ? `Loading ${state.value * 100}% complete` : "Loading..."}
+                        intent={state.intent}
+                        size={state.size}
+                        value={state.hasValue ? state.value : null}
                     />
-                </Card>
+                </Example>
                 <br/>
                 <h6 className="bp4-heading">Code </h6>
                 <p>
@@ -44,3 +109,23 @@ function SpinnerCore() {
 }
 
 export default SpinnerCore;
+
+function Reducer(state, action) {
+    switch (action.type) {
+        case 'IntentChange': {
+            return {...state, intent: action.value}
+        }
+
+        case 'BooleanChange': {
+            return {...state, hasValue: action.value}
+        }
+
+        case 'SizeChange': {
+            return {...state, size: action.value}
+        }
+
+        case 'ValueChange': {
+            return {...state, value: action.value}
+        }
+    }
+}
